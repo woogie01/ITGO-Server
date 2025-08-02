5c632335d41efd50376c01ce79f129b9cf0cebac
woogie01 <kwonhu3973@gmail.com>
2025-08-03 00:47:23 +0900

    [Feat] #1 : CurrentMemberId 어노테이션 추가

---
+++ src/main/java/likelion/itgo/global/resolver/CurrentMemberIdArgumentResolver.java
@@ -0,0 +1,33 @@
+package likelion.itgo.global.resolver;
+
+import likelion.itgo.global.jwt.dto.UserDetailsImpl;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class CurrentMemberIdArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(CurrentMemberId.class) != null
+                && parameter.getParameterType().equals(Long.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  org.springframework.web.bind.support.WebDataBinderFactory binderFactory) throws Exception {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetailsImpl userDetails) {
+            return userDetails.memberId();
+        }
+
+        return null;
+    }
+}
